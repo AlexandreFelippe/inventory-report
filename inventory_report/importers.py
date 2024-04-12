77fee12166ee7d6df46c25cf16a2ae6bf24b27f2
AlexandreFelippe <alemfelippe@gmail.com>
2024-04-12 11:14:58 -0300

    Requisito 03

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,16 +1,39 @@
-from typing import Dict, Type
+import csv
+from typing import Dict, Type, List
+from abc import ABC, abstractmethod
+from inventory_report.product import Product
+import json
 
 
-class Importer:
-    pass
+class Importer(ABC):
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> List[Product]:
+        raise NotImplementedError
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> List[Product]:
+        products = []
+        with open(self.path, 'r') as file:
+            data = json.load(file)
+            for item in data:
+                product = Product(**item)
+                products.append(product)
+        return products
 
 
-class CsvImporter:
-    pass
+class CsvImporter(Importer):
+    def import_data(self) -> List[Product]:
+        products = []
+        with open(self.path, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                product = Product(**row)
+                products.append(product)
+        return products
 
 
 # Não altere a variável abaixo
